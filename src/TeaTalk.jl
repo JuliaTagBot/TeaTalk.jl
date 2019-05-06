00c78c5dbbe1260d3912d169dd5e9fc0ea6bab16
Kristof Cools <krcools@gmail.com>
2019-05-06 17:51:22 +0200

    removed plotlyjs

--- src/TeaTalk.jl
+++ src/TeaTalk.jl
@@ -6,7 +6,6 @@ using Reexport
 @reexport using CompScienceMeshes
 @reexport using BEAST
 
-ENV["PLOTS_DEFAULT_BACKEND"] = "plotlyjs"
 @reexport using Plots
 
 Base.ones(::Type{T}, m::Array) where {T} = Base.ones(T, size(m))
