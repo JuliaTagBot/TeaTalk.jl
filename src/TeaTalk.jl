9051b05519a33189039737a09c21d6e3f53babe2
Kristof Cools <krcools@gmail.com>
2019-05-06 17:24:41 +0200

    ones for Array as 2nd arg

--- src/TeaTalk.jl
+++ src/TeaTalk.jl
@@ -5,8 +5,10 @@ using Reexport
 @reexport using LinearAlgebra
 @reexport using CompScienceMeshes
 @reexport using BEAST
+
+ENV["PLOTS_DEFAULT_BACKEND"] = "plotlyjs"
 @reexport using Plots
 
-Base.ones(::Type{T}, m::Matrix) where {T} = Base.ones(T, size(m))
+Base.ones(::Type{T}, m::Array) where {T} = Base.ones(T, size(m))
 
 end # module
