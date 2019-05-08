aa82708db7887c1e890046995b862bb2e76c650b
Kristof Cools <krcools@gmail.com>
2019-05-08 18:00:38 +0200

    now compatible with the older makie on nextjournal

--- examples/efie.jl
+++ examples/efie.jl
@@ -30,8 +30,8 @@ nfd = potential(N, gridpoints, u, X)
 nfd = reshape(nfd, size(gridpoints))
 nfd .-= E.(gridpoints)
 
-p1 = Makie.mesh(Γ,color=fc(norm.(fcr)),shading=false)
-p2 = scatter(Θ, real.(norm.(ffd)),xlabel="\\theta",legend=false)
-p3 = contour(clamp.(real.(norm.(nfd)), 0.0, 2.0))
-p4 = plot(real.(getindex.(nfd,2))[25,:],legend=false)
+p1 = Makie.mesh(Γ,color=fc(norm.(fcr)),shading=false);
+p2 = scatter(Θ, real.(norm.(ffd)),legend=false);
+p3 = heatmap(clamp.(real.(norm.(nfd)), 0.0, 2.0));
+p4 = lines(real.(getindex.(nfd,2))[25,:],legend=false);
 s = vbox(hbox(p2,p1),hbox(p4,p3))
--- examples/helmholtz.jl
+++ examples/helmholtz.jl
@@ -4,8 +4,6 @@ Id = TeaTalk.Identity()
 
 h = 0.05
 m = meshdisk(radius=1.0, h=h)
-v = skeleton(m,0)
-b = boundary(m)
 
 # X = lagrangec0d1(m, getindex.(cells(v),1), Val{3})
 X = lagrangec0d1(m)
@@ -17,3 +15,35 @@ p = sortperm(F.values)
 u = F.vectors[:,p[4]]
 fcr, geo = facecurrents(u,X)
 Makie.mesh(m, color=fc(getindex.(fcr,1)), colormap=:lighttest, shading=:false)
+
+
+h = 0.025
+m = meshdisk(radius=1.0, h=h)
+
+v = skeleton(m,0)
+b = boundary(m)
+X = lagrangec0d1(m, getindex.(cells(v),1), Val{3})
+@show numfunctions(X)
+x = strace(X,b)
+
+function f(x)
+    r = norm(x-point(0.3,0.3,0.0))
+    r0 = 0.1
+    return exp(-(r/r0)^2)/sqrt(r0)
+end
+
+g = TeaTalk.BEAST.ScalarTrace(f)
+κ = 8π
+trc = X->strace(X,b)
+
+@hilbertspace u
+@hilbertspace v
+Eq = @varform Id[curl(v),curl(u)] - κ^2*Id[v,u] - im*κ*Id[trc(v),trc(u)] == g[v]
+eq = @discretise Eq u∈X v∈X
+
+uh = solve(eq)
+
+fcr, geo = facecurrents(uh,X)
+c = clamp.(real.(getindex.(fcr,1)),-0.002,+0.002)
+# c = log10.(abs.(real.(getindex.(fcr,1))))
+Makie.mesh(m, color=fc(c), shading=false)
